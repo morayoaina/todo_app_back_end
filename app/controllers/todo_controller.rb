6d7be24563fb38bc62f15ade82073e03abcdea92
maina <morayo.aina@epicacademy.org>
2017-10-21 18:51:07 +0000

    Made conditional logic if statement to the show action of the Todo list

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,7 +3,15 @@ class TodoController < ApplicationController
     end
 
     def show
-        @todo_description = "Make a curriculm"
-        @todo_pomodoro_estimate = 4 
+        todo_id = params[:id]
+        if todo_id=='1'
+            @todo_description = "Make a curriculm"
+            @todo_pomodoro_estimate = 4 
+            
+        elsif todo_id == '2'
+         @todo_description = "Buy workshp supplies"
+        @todo_pomodoro_estimate = 3
+        
+        end
     end
 end
